810077d1f9afcf9de07ee3e8a2e62421fb3fbaaa
Markus Silvan <markus.silvan@iki.fi>
2023-02-25 02:02:55 +0200

    Remove change color system from splashscreen

--- src/splashscreen.rs
+++ src/splashscreen.rs
@@ -14,10 +14,6 @@ impl Plugin for SplashScreenPlugin {
                 std::time::Duration::from_millis(3000),
                 "splashscreen_delay",
             )
-            .add_fixed_timestep(
-                std::time::Duration::from_millis(300),
-                "splashscreen_effect_delay",
-            )
             .add_system_set(
                 SystemSet::on_enter(AppState::SplashScreen)
                     .with_system(spawn_splashscreen_system))
@@ -25,10 +21,6 @@ impl Plugin for SplashScreenPlugin {
                 "splashscreen_delay",
                 0,
                 start_game_system.run_if(in_splashscreen))
-            .add_fixed_timestep_system(
-                "splashscreen_effect_delay",
-                0,
-                change_color_system.run_if(in_splashscreen))
             .add_system_set(
                 SystemSet::on_exit(AppState::SplashScreen)
                     .with_system(despawn_splashscreen_system));
@@ -52,15 +44,6 @@ fn spawn_splashscreen_system(mut commands: Commands,
         },
         ..default()
     }).insert(BackgroundImage);
-}
-
-fn change_color_system(time: Res<Time>, mut query: Query<&mut Sprite>, state: ResMut<State<AppState>>) {
-    debug!("Running change color system in state: {:?}", state.current());
-    for mut sprite in &mut query {
-        sprite
-            .color
-            .set_b((time.elapsed_seconds() * 0.1).sin() + 2.0);
-    }
 }
 
 fn start_game_system(mut state: ResMut<State<AppState>>) {
